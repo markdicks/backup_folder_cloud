1039b05cd8a7b3fbb953bc9a051736c0a72d3e2d
Mark Dicks <117034609+markdicks@users.noreply.github.com>
2023-02-16 02:27:22 +0200

    Update upload_file.py

--- upload_file.py
+++ upload_file.py
@@ -1,48 +1,37 @@
-import os
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+import pickle
 
-# Authenticate with Google Drive API
-gauth = GoogleAuth()
-gauth.Auth()
+SCOPES = ['https://www.googleapis.com/auth/drive']
 
-# Get the authenticated GoogleDrive instance
-drive = GoogleDrive(gauth)
+creds = None
 
-# Ask user for the directory to back up
-folder_path = input("Enter the directory to back up: ")
-folder_name = os.path.basename(folder_path)
+try:
+    with open('token.pickle', 'rb') as token:
+        creds = pickle.load(token)
+except FileNotFoundError:
+    pass
 
-# Check if the folder already exists in Google Drive
-folder_exists = False
-file_list = drive.ListFile({'q': "trashed=false and mimeType='application/vnd.google-apps.folder' and title='" + folder_name + "'"}).GetList()
-if len(file_list) > 0:
-    folder_exists = True
-    folder_id = file_list[0]['id']
+if not creds or not creds.valid:
+    flow = InstalledAppFlow.from_client_config(
+        {"installed": {
+            "client_id": "203408454421-m7fngl1gaobp3thvb9hi6jr9j28lc958.apps.googleusercontent.com",
+            "project_id": "",
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://oauth2.googleapis.com/token",
+            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+            "client_secret": "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",
+            "redirect_uris": ["http://localhost:8080/"],
+            "javascript_origins": []
+        }},
+        scopes=SCOPES,
+            redirect_uri='http://localhost:8080/' #62941/
+        )
 
-# Create a new folder in Google Drive if it doesn't exist
-if not folder_exists:
-    folder = drive.CreateFile({'title': folder_name, 'mimeType': 'application/vnd.google-apps.folder'})
-    folder.Upload()
-    folder_id = folder['id']
-    print("New folder created in Google Drive with ID:", folder_id)
+    creds = flow.run_local_server(port=0)
 
-# Upload the directory to Google Drive
-folder_file = drive.CreateFile({'title': folder_name, 'parents': [{'id': folder_id}], 'mimeType': 'application/vnd.google-apps.folder'})
-folder_file.Upload()
+    with open('token.pickle', 'wb') as token:
+        pickle.dump(creds, token)
 
-for root, dirs, files in os.walk(folder_path):
-    for file in files:
-        file_path = os.path.join(root, file)
-        file_name = os.path.relpath(file_path, folder_path)
-        file_drive_list = drive.ListFile({'q': "trashed=false and mimeType!='application/vnd.google-apps.folder' and title='" + file_name + "' and parents in '" + folder_id + "'"}).GetList()
-        if len(file_drive_list) > 0:
-            file_drive = file_drive_list[0]
-            file_drive.SetContentFile(file_path)
-            file_drive.Upload()
-            print("Updated file in Google Drive:", file_name)
-        else:
-            file_drive = drive.CreateFile({'title': file_name, 'parents': [{'id': folder_id}]})
-            file_drive.SetContentFile(file_path)
-            file_drive.Upload()
-            print("Uploaded new file to Google Drive:", file_name)
+service = build('drive', 'v3', credentials=creds)
